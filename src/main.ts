1a371b38f90096387b18b26b730239a4f5513822
Gemma Centrone <gemma.centrone@student.unisi.it>
2023-01-23 13:19:15 +0100

    .

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,12 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { faPhone } from "@fortawesome/free-solid-svg-icons";
+import {
+  faHome,
+  faHeadphones,
+  faRandom,
+} from "@fortawesome/free-solid-svg-icons";
 
-library.add(faPhone);
+library.add(faHome, faHeadphones, faRandom);
 
-createApp(App)
-  .use(router)
-  .mount("#app");
+createApp(App).use(router).mount("#app");
